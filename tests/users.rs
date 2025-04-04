0b531969bbb7b14d29ab91ddff886a006fdc68bc
reuben olinsky <reubeno@users.noreply.github.com>
2025-04-05 00:06:44 +0200

    test: add test_all_users and test_all_groups

--- tests/users.rs
+++ tests/users.rs
@@ -7,6 +7,7 @@ mod integration {
     use uzers::os::unix::UserExt;
 
     #[test]
+    #[serial_test::serial]
     fn test_user_by_name() {
         let user = uzers::get_user_by_name("fred");
 
@@ -14,6 +15,19 @@ mod integration {
 
         let user = user.unwrap();
 
+        assert_eq!(user.uid(), 1337);
+        assert_eq!(user.name(), "fred");
+        assert_eq!(user.primary_group_id(), 42);
+        assert_eq!(user.home_dir(), PathBuf::from("/home/fred"));
+    }
+
+    #[test]
+    #[serial_test::serial]
+    fn test_all_users() {
+        let users: Vec<_> = unsafe { uzers::all_users() }.collect();
+        assert_eq!(users.len(), 1);
+
+        let user = users.first().unwrap();
         assert_eq!(user.uid(), 1337);
         assert_eq!(user.name(), "fred");
         assert_eq!(user.primary_group_id(), 42);
