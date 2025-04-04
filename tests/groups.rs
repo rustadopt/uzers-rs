1366bbd3800e5acdd1065db882cbd8d65ec6a5b1
reuben olinsky <reubeno@users.noreply.github.com>
2025-04-05 00:06:44 +0200

    test: add test_all_users and test_all_groups

--- tests/groups.rs
+++ tests/groups.rs
@@ -3,6 +3,7 @@ extern crate uzers;
 #[cfg(feature = "test-integration")]
 mod integration {
     #[test]
+    #[serial_test::serial]
     fn test_group_by_name() {
         let group = uzers::get_group_by_name("bosses");
 
@@ -12,5 +13,20 @@ mod integration {
 
         assert_eq!(group.gid(), 42);
         assert_eq!(group.name(), "bosses");
+    }
+
+    #[test]
+    #[serial_test::serial]
+    fn test_all_groups() {
+        let groups: Vec<_> = unsafe { uzers::all_groups() }.collect();
+        assert_eq!(groups.len(), 2);
+
+        let group = &groups[0];
+        assert_eq!(group.gid(), 42);
+        assert_eq!(group.name(), "bosses");
+
+        let group = &groups[1];
+        assert_eq!(group.gid(), 43);
+        assert_eq!(group.name(), "contributors");
     }
 }
